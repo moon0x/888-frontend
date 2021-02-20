4705da3a64624a4d25265ae26664c885596bcb9d
Ahmed Ali <imahmedalibhatti@gmail.com>
2021-02-20 09:49:32 +0500

    added mainnet nodes

--- src/config/getRpcUrl.js
+++ src/config/getRpcUrl.js
@@ -8,9 +8,23 @@ const testNodes = [
   'https://data-seed-prebsc-1-s2.binance.org:8545/',
   'https://data-seed-prebsc-2-s2.binance.org:8545/',
 ];
-const getRpcUrl = () => {
-  const randomIndex = random(0, testNodes.length - 1);
-  return testNodes[randomIndex];
+
+const mainNode = [
+  'https://bsc-dataseed.binance.org/',
+  'https://bsc-dataseed1.defibit.io/',
+  'https://bsc-dataseed1.ninicoin.io/',
+  'https://bsc-dataseed2.defibit.io/',
+  'https://bsc-dataseed3.defibit.io/',
+];
+
+const getRpcUrl = (chainId) => {
+  if(chainId === '65') {
+    const randomIndex = random(0, mainNode.length - 1);
+    return mainNode[randomIndex];
+  } else {
+    const randomIndex = random(0, testNodes.length - 1);
+    return testNodes[randomIndex];
+  }
 }
 
 export default getRpcUrl;
